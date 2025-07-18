6896d58cd6c8a288c819092d04042691375f1dc0
jenny870604 <60332942+jenny870604@users.noreply.github.com>
2025-07-18 23:22:02 +0800

    main test

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+# from fastapi import FastAPI
+# import pandas as pd
+# import os
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Welcome to Population Forecast API"}
+
+# @app.get("/forecast")
+# def get_forecast():
+#     base_dir = os.path.dirname(__file__)  # 這時是根目錄
+#     pkl_path = os.path.join(base_dir, "2024_人口預測.pkl")
+#     df = pd.read_pickle(pkl_path)
+#     return df.to_dict(orient="records")
 from fastapi import FastAPI
-import pandas as pd
-import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to Population Forecast API"}
+    return {"message": "Hello from Railway"}
 
-@app.get("/forecast")
-def get_forecast():
-    base_dir = os.path.dirname(__file__)  # 這時是根目錄
-    pkl_path = os.path.join(base_dir, "2024_人口預測.pkl")
-    df = pd.read_pickle(pkl_path)
-    return df.to_dict(orient="records")
