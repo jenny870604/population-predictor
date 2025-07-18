97a0aa5136c5df4bbe32f8959d45638fa6e2082f
jenny870604 <60332942+jenny870604@users.noreply.github.com>
2025-07-18 23:39:45 +0800

    pkl改csv

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to Population Forecast API"}
 
+# @app.get("/forecast")
+# def get_forecast():
+#     base_dir = os.path.dirname(__file__)  # 這時是根目錄
+#     pkl_path = os.path.join(base_dir, "2024_人口預測.pkl")
+#     df = pd.read_pickle(pkl_path)
+#     return df.to_dict(orient="records")
 @app.get("/forecast")
 def get_forecast():
-    base_dir = os.path.dirname(__file__)  # 這時是根目錄
-    pkl_path = os.path.join(base_dir, "2024_人口預測.pkl")
-    df = pd.read_pickle(pkl_path)
+    csv_path = os.path.join(os.path.dirname(__file__), "2024_人口預測.csv")
+    df = pd.read_csv(csv_path)
     return df.to_dict(orient="records")
